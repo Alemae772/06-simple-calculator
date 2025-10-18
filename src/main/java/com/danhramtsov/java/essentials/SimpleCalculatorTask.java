bc11e57b0b6a5720f50c5c023092abb9e336eb9c
Ale mae <deadun99@gmail.com>
2025-10-18 15:37:13 +0300

    1

--- src/main/java/com/danhramtsov/java/essentials/SimpleCalculatorTask.java
+++ src/main/java/com/danhramtsov/java/essentials/SimpleCalculatorTask.java
@@ -1,6 +1,27 @@
 package com.danhramtsov.java.essentials;
 
+import java.util.Scanner;
+
 public class SimpleCalculatorTask {
     //TODO: реализуйте код ниже
+            public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Введите число 1: ");
+            int a = sc.nextInt();
+            System.out.print("Введите знак: ");
+                int x= sc.nextInt();
+                System.out.print("Введите число 2: ");
+                int b = sc.nextInt();
+            if (x=*),  {System.out.printf(a*b);
+               else
+
+                System.out.printf("Число " + num + " четное");
+            }
+            else {
+                System.out.printf("Число " + num + " нечетное");
+            }
+            sc.close();
+        }
+    }
 
 }
